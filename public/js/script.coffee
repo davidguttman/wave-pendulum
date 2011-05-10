5ce0efa09dd9515ca57ce4fa3cbff4bfb8d78d68
David Guttman <david@davidguttman.com>
2011-05-10 16:47:09 -0700

    working well

--- public/js/script.coffee
+++ public/js/script.coffee
@@ -2,7 +2,9 @@ coffee_draw = (p5) ->
   p5.setup = ->
     p5.size $(window).width(), $(window).height()
     p5.background 0
-    p5.balls = []
+    p5.n_balls = 32
+    p5.reset_balls()
+    p5.frameRate(60)
     
   p5.draw = ->
     p5.fade()
@@ -12,24 +14,41 @@ coffee_draw = (p5) ->
 
   p5.fade = ->
     p5.stroke(0, 0)
-    p5.fill(0, 50)
+    p5.fill(0, 80)
     p5.rect(0, 0, p5.width, p5.height)
   
   p5.mouseClicked = ->
-    p5.balls.push new Ball p5, 
-      o_x: p5.mouseX
-      o_y: p5.mouseY
+    p5.reset_balls()
+    # p5.balls.push new Ball p5, 
+    #   o_x: p5.mouseX
+    #   o_y: p5.mouseY
+      
+  p5.reset_balls = ->
+    p5.balls = []
+    space = p5.height/p5.n_balls
+    l = p5.width/3
+    for num in [1..p5.n_balls]
+      do (num) =>
+        p5.balls.push new Ball p5,
+          o_x: p5.width/2
+          o_y: space * num - (space/2)
+          b_x: l
+          r: l * Math.pow((16/(16+(num-1))),2)
 
 class Ball
   constructor: (@p5, opts) ->
     @o_x = opts.o_x
     @o_y = opts.o_y
 
-    @r = 100
+    @b_x = opts.b_x
+
+    @r = opts.r || 100
     @G = 0.4    
     @theta = Math.PI/2
     
-    @ball_r = 20
+    # (@b_x - @o_x)/@r = Math.sin(@theta)
+    
+    @ball_r = 12
         
     @vel = opts.vel || 0
     @accel = opts.accel || 0
@@ -37,7 +56,7 @@ class Ball
   draw: ->  
     @accel = (-1 * @G / @r) * Math.sin(@theta)
     @vel += @accel
-    @vel *= 0.999
+    @vel *= 0.9999
     @theta += @vel
     
     @b_x = @o_x + (@r * Math.sin(@theta))
--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,9 @@
     p5.setup = function() {
       p5.size($(window).width(), $(window).height());
       p5.background(0);
-      return p5.balls = [];
+      p5.n_balls = 32;
+      p5.reset_balls();
+      return p5.frameRate(60);
     };
     p5.draw = function() {
       var ball, _i, _len, _ref, _results;
@@ -22,14 +24,29 @@
     };
     p5.fade = function() {
       p5.stroke(0, 0);
-      p5.fill(0, 50);
+      p5.fill(0, 80);
       return p5.rect(0, 0, p5.width, p5.height);
     };
-    return p5.mouseClicked = function() {
-      return p5.balls.push(new Ball(p5, {
-        o_x: p5.mouseX,
-        o_y: p5.mouseY
-      }));
+    p5.mouseClicked = function() {
+      return p5.reset_balls();
+    };
+    return p5.reset_balls = function() {
+      var l, num, space, _ref, _results;
+      p5.balls = [];
+      space = p5.height / p5.n_balls;
+      l = p5.width / 3;
+      _results = [];
+      for (num = 1, _ref = p5.n_balls; 1 <= _ref ? num <= _ref : num >= _ref; 1 <= _ref ? num++ : num--) {
+        _results.push(__bind(function(num) {
+          return p5.balls.push(new Ball(p5, {
+            o_x: p5.width / 2,
+            o_y: space * num - (space / 2),
+            b_x: l,
+            r: l * Math.pow(16 / (16 + (num - 1)), 2)
+          }));
+        }, this)(num));
+      }
+      return _results;
     };
   };
   Ball = (function() {
@@ -37,17 +54,18 @@
       this.p5 = p5;
       this.o_x = opts.o_x;
       this.o_y = opts.o_y;
-      this.r = 100;
+      this.b_x = opts.b_x;
+      this.r = opts.r || 100;
       this.G = 0.4;
       this.theta = Math.PI / 2;
-      this.ball_r = 20;
+      this.ball_r = 12;
       this.vel = opts.vel || 0;
       this.accel = opts.accel || 0;
     }
     Ball.prototype.draw = function() {
       this.accel = (-1 * this.G / this.r) * Math.sin(this.theta);
       this.vel += this.accel;
-      this.vel *= 0.999;
+      this.vel *= 0.9999;
       this.theta += this.vel;
       this.b_x = this.o_x + (this.r * Math.sin(this.theta));
       this.b_y = this.o_y + (this.r * Math.cos(this.theta));
