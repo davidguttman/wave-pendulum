39b70a201b4c8dea28079c6a024b654c761674cc
David Guttman <david@davidguttman.com>
2011-05-11 13:06:59 -0700

    some nice y

--- public/js/script.coffee
+++ public/js/script.coffee
@@ -25,13 +25,13 @@ coffee_draw = (p5) ->
       
   p5.reset_balls = ->
     p5.balls = []
-    space = p5.height/(p5.n_balls*1)
     l = p5.width/3
+    space = (p5.height-l/2)/(p5.n_balls)
     for num in [1..p5.n_balls]
       do (num) =>
         p5.balls.push new Ball p5,
           o_x: p5.width/2
-          o_y: space * num - (space/2)
+          o_y: space * num - (space)
           b_x: l
           r: l * Math.pow((16/(16+(num-1))),2)
 
@@ -43,8 +43,8 @@ class Ball
     @b_x = opts.b_x
 
     @r = opts.r || 100
-    @G = 0.14    
-    @theta = Math.PI/2
+    @G = 0.2
+    @theta = 0.8*Math.PI/2
     
     # (@b_x - @o_x)/@r = Math.sin(@theta)
     
@@ -65,12 +65,12 @@ class Ball
     @b_x = @o_x + @x_off
     @b_y = @o_y + @y_off
     
-    bright = 255 - ((@b_y - @o_y)/@r * 128)
     
     x = (@x_off/@r)*@p5.height/4 + @o_x
+    y = (@y_off/@r)*@p5.height/4 + @o_y
     
     @p5.fill(255)
-    @p5.ellipse(x, @o_y, @ball_r, @ball_r)
+    @p5.ellipse(x, y, @ball_r, @ball_r)
 
 $(document).ready ->
   canvas = document.getElementById "processing"
--- public/js/script.js
+++ public/js/script.js
@@ -33,14 +33,14 @@
     return p5.reset_balls = function() {
       var l, num, space, _ref, _results;
       p5.balls = [];
-      space = p5.height / (p5.n_balls * 1);
       l = p5.width / 3;
+      space = (p5.height - l / 2) / p5.n_balls;
       _results = [];
       for (num = 1, _ref = p5.n_balls; 1 <= _ref ? num <= _ref : num >= _ref; 1 <= _ref ? num++ : num--) {
         _results.push(__bind(function(num) {
           return p5.balls.push(new Ball(p5, {
             o_x: p5.width / 2,
-            o_y: space * num - (space / 2),
+            o_y: space * num - space,
             b_x: l,
             r: l * Math.pow(16 / (16 + (num - 1)), 2)
           }));
@@ -56,14 +56,14 @@
       this.o_y = opts.o_y;
       this.b_x = opts.b_x;
       this.r = opts.r || 100;
-      this.G = 0.14;
-      this.theta = Math.PI / 2;
+      this.G = 0.2;
+      this.theta = 0.8 * Math.PI / 2;
       this.ball_r = 12;
       this.vel = opts.vel || 0;
       this.accel = opts.accel || 0;
     }
     Ball.prototype.draw = function() {
-      var bright, x;
+      var x, y;
       this.accel = (-1 * this.G / this.r) * Math.sin(this.theta);
       this.vel += this.accel;
       this.vel *= 1;
@@ -72,10 +72,10 @@
       this.y_off = this.r * Math.cos(this.theta);
       this.b_x = this.o_x + this.x_off;
       this.b_y = this.o_y + this.y_off;
-      bright = 255 - ((this.b_y - this.o_y) / this.r * 128);
       x = (this.x_off / this.r) * this.p5.height / 4 + this.o_x;
+      y = (this.y_off / this.r) * this.p5.height / 4 + this.o_y;
       this.p5.fill(255);
-      return this.p5.ellipse(x, this.o_y, this.ball_r, this.ball_r);
+      return this.p5.ellipse(x, y, this.ball_r, this.ball_r);
     };
     return Ball;
   })();
